0500b774fc74b362fbd858146bc5030be03b706f
Carlos Díaz <linkmetal18@gmail.com>
2021-08-28 16:23:47 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.amber,
+      ),
+      home: RandomWords()
+          );
+            // home: Scaffold(
+            //   appBar: AppBar(title: Text('Random wordpairs')),
+            //   body: Center(child: Text(wordPair.asPascalCase))
+            // ),
+        }
+      }
+      
+  class RandomWords extends StatefulWidget {
+    @override 
+    State<StatefulWidget> createState() => RandomWordsState();
+}
