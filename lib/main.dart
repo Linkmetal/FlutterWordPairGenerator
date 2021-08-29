cca1fc4c2e2d2f1ab1aba43451c4f220ca486df0
Carlos Díaz <linkmetal18@gmail.com>
2021-08-29 13:54:58 +0200

    Added saved words page

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,109 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.amber,
-      ),
-      home: RandomWords()
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.amber,
+        ),
+        home: RandomWords());
+    // home: Scaffold(
+    //   appBar: AppBar(title: Text('Random wordpairs')),
+    //   body: Center(child: Text(wordPair.asPascalCase))
+    // ),
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+  final _saved = <WordPair>{};
+  void _pushToSaved() {
+Navigator.of(context).push(
+  MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                ),
+              );
+            },
           );
-            // home: Scaffold(
-            //   appBar: AppBar(title: Text('Random wordpairs')),
-            //   body: Center(child: Text(wordPair.asPascalCase))
-            // ),
-        }
-      }
-      
-  class RandomWords extends StatefulWidget {
-    @override 
-    State<StatefulWidget> createState() => RandomWordsState();
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+  ));
+  }
+
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          final index = item ~/ 2;
+
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_randomWordPairs[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget build(BuildContext constext) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wordpair generator'),
+        actions: [IconButton(onPressed: _pushToSaved, icon: Icon(Icons.list))],
+      ),
+      body: _buildList(),
+    );
+  }
 }
